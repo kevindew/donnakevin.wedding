59ebb3b82aafc3d4a5904a0bfac9007bd64c33a6
Kevin Dew <kevindew@me.com>
2019-08-11 16:28:59 +0100

    Add building blocks towards this being a website
    I couldn't work out how to reference the CSS file in the HTML rather
    than JS which was a bit annoying but oh well.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,18 +1,48 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
-export default {
-  entry: './src/js/main.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].[hash].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true
-      }
-    })
-  ]
+export default (_env, argv) => {
+  return {
+    entry: './src/main.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: '[name].[contenthash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            argv.mode == 'production'
+             ? MiniCssExtractPlugin.loader
+             : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.woff2$/,
+          use: ['url-loader'],
+        },
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+        }
+      }),
+    ]
+  };
 };
