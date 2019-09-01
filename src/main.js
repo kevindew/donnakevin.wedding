e660f4f418208d933d025136104c80c11ed4e501
Kevin Dew <kevindew@me.com>
2019-09-01 14:07:39 +0100

    Maybe finished nav menu

--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
 import './style.css'
 
-const pageNavLinks = [
-  document.querySelector('.page-nav__trigger'),
-  ...document.querySelectorAll('.page-nav__link')
-]
-
 const pageNav = document.querySelector('.page-nav')
 
-for (const navLink of pageNavLinks) {
+document.querySelector('.page-nav__trigger').addEventListener('click', (event) => {
+  event.preventDefault()
+  pageNav.classList.toggle('page-nav--open')
+})
+
+for (const navLink of document.querySelectorAll('.page-nav__link')) {
   navLink.addEventListener('focus', (event) => {
     pageNav.classList.add('page-nav--open')
   })
 
   navLink.addEventListener('click', (event) => {
-    if (event.currentTarget.classList.contains('page-nav__trigger')) {
-      event.preventDefault()
-    }
-
-    pageNav.classList.toggle('page-nav--open')
+    pageNav.classList.remove('page-nav--open')
   })
 }
 
@@ -31,3 +27,4 @@ const bodyHandler = (event) => {
 
 document.querySelector('body').addEventListener('click', bodyHandler)
 document.querySelector('body').addEventListener('focusin', bodyHandler)
+document.querySelector('body').addEventListener('touchstart', bodyHandler)
