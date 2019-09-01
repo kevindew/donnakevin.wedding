9eef71541dbf3ad577ad69b4570054c4087c86a7
Kevin Dew <kevindew@me.com>
2019-09-01 11:30:05 +0100

    Improved (but unfinished) nav menu

--- src/main.js
+++ src/main.js
@@ -1,21 +1,33 @@
 import './style.css'
-// polyfill for IE Element.prototype.closest
-import 'element-closest'
 
 const pageNavLinks = [
   document.querySelector('.page-nav__trigger'),
   ...document.querySelectorAll('.page-nav__link')
 ]
 
+const pageNav = document.querySelector('.page-nav')
+
 for (const navLink of pageNavLinks) {
+  navLink.addEventListener('focus', (event) => {
+    pageNav.classList.add('page-nav--open')
+  })
+
   navLink.addEventListener('click', (event) => {
     if (event.currentTarget.classList.contains('page-nav__trigger')) {
       event.preventDefault()
     }
 
-    const nav = event.currentTarget.closest('.page-nav')
-    if (nav) {
-      nav.classList.toggle('page-nav--open')
-    }
+    pageNav.classList.toggle('page-nav--open')
   })
 }
+
+const bodyHandler = (event) => {
+  if (pageNav.classList.contains('page-nav--open')) {
+    if (!event.target || !pageNav.contains(event.target)) {
+      pageNav.classList.remove('page-nav--open')
+    }
+  }
+}
+
+document.querySelector('body').addEventListener('click', bodyHandler)
+document.querySelector('body').addEventListener('focusin', bodyHandler)
