4e76c6e42ac7ed34971bc1abd420a26a8c0fb93a
Kevin Dew <kevindew@me.com>
2019-09-26 20:32:10 +0100

    Re-order JS comments

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+// CSS needs to be here for some webpack reason
+import './style.css'
+
 // polyfills for IE11
 import 'element-closest'
 import 'promise-polyfill/src/polyfill'
 import 'whatwg-fetch'
 
-import './style.css'
 import './menu'
 import './rsvp'
