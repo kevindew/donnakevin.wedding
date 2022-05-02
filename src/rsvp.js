117fc59cb8eed46ef67b9500678200667be66500
Kevin Dew <kevindew@me.com>
2022-05-02 11:47:52 +0100

    Stop accepting RSVP's and gifts.
    The wedding has already happened, but this hasn't stopped the spam bots.

--- src/rsvp.js
+++ src/rsvp.js
@@ -28,7 +28,6 @@ form.addEventListener('submit', event => {
   }
   formSubmitting = true
 
-  hideFormFailed()
   const valid = validateRsvp()
 
   if (valid) {
@@ -36,29 +35,12 @@ form.addEventListener('submit', event => {
     button.dataset.original = button.textContent
     button.textContent = 'Sending...'
     button.disabled = true
-    sendFormData()
-      .then(response => {
-        if (response.ok) {
-          return response
-        }
-
-        throw new Error(response)
-      })
-      .then(() => {
-        const oldWrapper = form.closest('.wrapper')
-        const rsvpThanks = document.querySelector('#rsvp-thanks')
-        const newWrapper = rsvpThanks.querySelector('.wrapper')
-        oldWrapper.parentNode.replaceChild(newWrapper, oldWrapper)
-        rsvpThanks.parentNode.removeChild(rsvpThanks)
-        newWrapper.scrollIntoView({ behavior: 'smooth' })
-      })
-      .catch(error => {
-        console.error(error)
-        showFormFailed()
-        button.textContent = button.dataset.original
-        button.disabled = false
-        formSubmitting = false
-      })
+    const oldWrapper = form.closest('.wrapper')
+    const rsvpOver = document.querySelector('#rsvp-over')
+    const newWrapper = rsvpOver.querySelector('.wrapper')
+    oldWrapper.parentNode.replaceChild(newWrapper, oldWrapper)
+    rsvpOver.parentNode.removeChild(rsvpOver)
+    newWrapper.scrollIntoView({ behavior: 'smooth' })
   } else {
     formSubmitting = false
   }
@@ -91,50 +73,4 @@ function validateRsvp () {
   }
 
   return valid
-}
-
-function showFormFailed () {
-  const el = document.createElement('div')
-  el.id = 'rsvp-failed'
-  el.setAttribute('role', 'alert')
-  el.tabIndex = -1
-  el.classList.add('rsvp-failed')
-  el.innerHTML = `
-    <h3 class="f3 tc mv3">Oops, something went wrong</h3>
-    <p class="section-copy tc mv3">
-      We didn't receive your response, please try again or
-      email us at
-      <a href="mailto:help@donnakevin.wedding?subject=Wedding%20RSVP"
-         class="link link--error">help@donnakevin.wedding</a>
-      </a>
-    </p>
-  `
-
-  form.parentNode.insertBefore(el, form)
-  el.focus()
-}
-
-function hideFormFailed () {
-  const wrapper = form.parentNode
-  const formFailed = wrapper.querySelector('#rsvp-failed')
-  if (formFailed) {
-    wrapper.removeChild(formFailed)
-  }
-}
-
-function sendFormData () {
-  const data = {}
-  const formData = new window.FormData(form)
-  for (const [key, value] of formData.entries()) {
-    data[key] = value
-  }
-
-  return window.fetch(form.action, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
 }
